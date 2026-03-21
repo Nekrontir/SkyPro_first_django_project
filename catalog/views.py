068056363d1b64dfb0f4a6a228b842d1bc18bdd4
Viktor Schastlivenko <klarkkent0314@gmail.com>
2026-03-21 21:31:51 +0300

    Homework 24

--- catalog/views.py
+++ catalog/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
 
 def home(request):
-    return render(request, "home.html")
+    products = Product.objects.all()
+    return render(request, "home.html", {"products": products})
 
 
 def contacts(request):
     return render(request, "contacts.html")
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, "product_detail.html", {"product": product})
