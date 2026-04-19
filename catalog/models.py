ad0d84d4cc6aaaaa78d8dc0a7c828fffba29077c
Viktor <klarkkent0314@gmail.com>
2026-04-19 11:29:18 +0300

    start homework 28

--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.conf import settings
 
 class Category(models.Model):
     """
@@ -32,7 +32,14 @@ class Product(models.Model):
     purchase_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата последнего изменения")
-    is_published = models.BooleanField(default=True, verbose_name="Опубликован")
+    is_published = models.BooleanField(default=False, verbose_name="Опубликован")
+
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='products',
+        verbose_name="Владелец"
+    )
 
     def __str__(self):
         """
@@ -43,3 +50,6 @@ class Product(models.Model):
     class Meta:
         verbose_name = "Продукт"
         verbose_name_plural = "Продукты"
+        permissions = [
+            ("can_unpublish_product", "Can unpublish product"),
+        ]
