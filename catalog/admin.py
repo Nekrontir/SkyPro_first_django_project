41c16e41d206ab3bcfa7a9d43dd641192f75beab
Viktor <klarkkent0314@gmail.com>
2026-04-19 12:13:55 +0300

    working

--- catalog/admin.py
+++ catalog/admin.py
@@ -10,6 +10,6 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "purchase_price", "category")
-    list_filter = ("category",)
+    list_display = ("id", "name", "purchase_price", "category", "owner", "is_published")
+    list_filter = ("category", "is_published")
     search_fields = ("name", "description")
--- catalog/urls.py
+++ catalog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from catalog.apps import CatalogConfig
 from catalog.views import (ContactsView, ProductCreateView, ProductDeleteView, ProductDetailView, ProductListView,
-                           ProductUpdateView)
+                           ProductUpdateView,unpublish_product)
 
 app_name = CatalogConfig.name
 
@@ -13,4 +13,5 @@ urlpatterns = [
     path("product/create/", ProductCreateView.as_view(), name="product_create"),
     path("product/<int:pk>/update/", ProductUpdateView.as_view(), name="product_update"),
     path("product/<int:pk>/delete/", ProductDeleteView.as_view(), name="product_delete"),
+    path('product/<int:pk>/unpublish/', unpublish_product, name='product_unpublish'),
 ]
