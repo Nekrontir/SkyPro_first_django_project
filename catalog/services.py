ae4bd55a54ec803b3d3c79cca57a479852cc6d99
Viktor <klarkkent0314@gmail.com>
2026-04-26 09:53:30 +0300

    add cache decorator in urls.py, created services.py

---
+++ catalog/services.py
@@ -0,0 +1,8 @@
+from catalog.models import Product
+
+def get_products_by_category(category_id):
+    """Возвращает QuerySet опубликованных продуктов указанной категории."""
+    return Product.objects.filter(
+        category_id=category_id,
+        is_published=True
+    ).select_related('category')
--- catalog/urls.py
+++ catalog/urls.py
@@ -4,12 +4,14 @@ from catalog.apps import CatalogConfig
 from catalog.views import (ContactsView, ProductCreateView, ProductDeleteView, ProductDetailView, ProductListView,
                            ProductUpdateView,unpublish_product)
 
+from django.views.decorators.cache import cache_page
+
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path("", ProductListView.as_view(), name="home"),
     path("contacts/", ContactsView.as_view(), name="contacts"),
-    path("product/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
+    path("product/<int:pk>/",cache_page(60 * 15)(ProductDetailView.as_view()), name="product_detail"),
     path("product/create/", ProductCreateView.as_view(), name="product_create"),
     path("product/<int:pk>/update/", ProductUpdateView.as_view(), name="product_update"),
     path("product/<int:pk>/delete/", ProductDeleteView.as_view(), name="product_delete"),
